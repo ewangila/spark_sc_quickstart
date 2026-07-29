a2c3bc1f9a842902717636e736cf581606ea6ee7
Eugin Wangila <eugenefarah@gmail.com>
2026-07-29 09:50:14 +0300

    version1

---
+++ spark.py
@@ -0,0 +1,39 @@
+from pyspark import SparkContext
+from datetime import datetime
+
+# Create SparkContext
+sc = SparkContext("local[*]", "learning")
+
+# BASIC INFO
+print("Basic Info")
+print(f"Type: {type(sc)}")
+print(f"App Name: {sc.appName}")
+print(f"Master: {sc.master}")
+print(f"Version: {sc.version}")
+
+# STARTUP INFO 
+print("\nStartup Info")
+print(f"Start Time (raw): {sc.startTime}")
+readable_time = datetime.fromtimestamp(sc.startTime / 1000)
+print(f"Start Time (readable): {readable_time}")
+
+#  RESOURCE INFO 
+print("\nResource Info")
+print(f"Default Parallelism: {sc.defaultParallelism}")
+print(f"Default Min Partitions: {sc.defaultMinPartitions}")
+
+# USER & SYSTEM INFO 
+print("\nUser & System Info")
+print(f"Spark User: {sc.sparkUser}")
+print(f"Spark Home: {sc.sparkHome}")
+print(f"Application ID: {sc.applicationId}")
+
+# UI & STATUS 
+
+print("\nUI & Status")
+print(f"UI Web URL: {sc.uiWebUrl}")
+
+
+
+# Clean up
+sc.stop()
